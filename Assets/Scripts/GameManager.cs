3eebdd9d978715382f01597aa434e8614df84472
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-09-14 14:34:42 -0700

    Singleton gamemanager & multiple wagons

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    int score = 0;
+    public int enemyCount = 0;
+
+    [SerializeField] GameObject levelPrefab;
+    [SerializeField] GameObject emptyNextLevelPrefab;
+    [SerializeField] GameObject currentLevel;
+    [SerializeField] GameObject nextLevel;
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*currentLevel = Instantiate(currentLevel, new Vector3(0, 0), quaternion.identity);
+        GameObject muroDerecha = currentLevel.transform.Find("Muro derecha").gameObject;
+        Vector3 nextLevelPosition = new Vector3(muroDerecha.transform.position.x + 12, 0);
+        nextLevel = Instantiate(nextLevel, nextLevelPosition, quaternion.identity);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
