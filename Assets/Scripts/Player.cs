237389c15e707c95fbef5c0376983c2d40241d8f
Prikense <a01251446@itesm.mx>
2024-09-14 01:10:11 -0600

    player movement improvements (friction), aiming and camera movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
 
+    [Header("Movement")]
     [SerializeField]
     public float speed;
+    [SerializeField]
+    public float coeficient;
+    [SerializeField]
+    public float maxSpeed;
+    [SerializeField]
+    public float dashMulti;
 
-    UnityEngine.Vector2 direction = UnityEngine.Vector2.zero;
+    Vector2 direction = Vector2.zero;
 
     public InputAction move;
+
+    [SerializeField]
+    private Vector2 inertia;
+
+
+    [Header("Camera")]
+    [SerializeField]
+    Camera playerCam;
+    [SerializeField]
+    Vector3 mouseVec;
+    [SerializeField]
+    Vector3 camBasePos;
+    [SerializeField]
+    float camSpeed;
+    [SerializeField]
+    float maxCamDistance;
+    [Header("Visuals")]
+    [SerializeField]
+    SpriteRenderer BodySprite;
 
     private void OnEnable(){
         move.Enable();
@@ -29,13 +55,49 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        direction = move.ReadValue<UnityEngine.Vector2>();
+        direction = move.ReadValue<Vector2>();
+
+        mouseVec = playerCam.ScreenToWorldPoint(Input.mousePosition);
         
     }
 
     void FixedUpdate()
     {
-        UnityEngine.Vector2 movement= new UnityEngine.Vector2(direction.x,direction.y);
-        transform.Translate(movement*speed*Time.deltaTime);
+        MoveTime();
+
+        MouseTime();
+    }
+
+    private void MouseTime() {
+        Vector3 lookDir = new Vector3(mouseVec.x-transform.position.x, mouseVec.y-transform.position.y, 0); 
+        float distnace = lookDir.magnitude;
+        lookDir = lookDir.normalized;
+        Vector3 newCamPos = camBasePos+Vector3.ClampMagnitude(lookDir*distnace, maxCamDistance);
+        playerCam.transform.localPosition = Vector3.Lerp(camBasePos, newCamPos, camSpeed*Time.fixedDeltaTime);
+
+        print(lookDir);
+        print(transform.right);
+        print(Vector3.right);
+        BodySprite.transform.rotation = Quaternion.LookRotation(transform.forward, lookDir);
+        //to lerp the gun
+        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(body.velocity), rotAlpha);
+    }
+
+    private void MoveTime() {
+        Vector2 movement= new Vector2(direction.x,direction.y);
+        float trueSpeed = speed;
+        if(Mathf.Abs(inertia.magnitude) <= 3){
+            trueSpeed*=dashMulti;
+        }
+        //we round speed if its greater than maxspeed
+        inertia = Vector3.ClampMagnitude(inertia+movement*trueSpeed, maxSpeed);
+
+        Vector2 friction = inertia.normalized*-1*coeficient;
+        inertia +=friction;
+        //if friction would give a negative, instead we round to zero
+        if(Mathf.Abs(inertia.magnitude) <= 3) {
+            inertia= Vector2.zero;
+        }
+        transform.Translate(inertia*Time.deltaTime);
     }
 }
